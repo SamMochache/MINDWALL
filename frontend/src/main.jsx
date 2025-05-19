5acc941238653ebca3dfb6a94b1e6f165fa0f9fa
SamMochache <sammochache01@gmail.com>
2025-05-19 19:36:30 +0300

    Update axios base URL to use environment variable for API configuration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
 // Setup default axios behavior
-axios.defaults.baseURL = 'http://localhost:8000/';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
